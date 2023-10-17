b53be7fdcf9abcacf19fe0fbdc2895d2552111ef
juan francisco gonzalez cerrato <jfgonzalezc02@gmail.com>
2023-10-17 13:16:53 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function lluviaDePajaros() {
+	let pajarito = document.createElement("img");
+	pajarito.src = "image (2).png";
+	pajarito.className = "element";
+	pajarito.style.left = `${1 + Math.random() * 80}%`;
+	let tamañoAleatorio = 50 + Math.random() * 100; 
+	pajarito.style.width = `${tamañoAleatorio}px`;
+	pajarito.style.height = "auto";
+	document.querySelector("body").appendChild(pajarito);
+	setTimeout(() => {
+		pajarito.remove();
+	}, 5000);
+}
+
+setInterval(lluviaDePajaros, 1000);
